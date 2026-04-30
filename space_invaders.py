ca19d24629ae2899b97353f273b3ccb169e2ffad
UnaOwl-jetlearn <una.pooky@gmail.com>
2026-04-30 14:58:49 +0300

    Add files via upload

--- space_invaders.py
+++ space_invaders.py
@@ -12,24 +12,54 @@ ship2 = pygame.image.load("space_invaders/images/yellow_ship.png")
 red_ship = pygame.transform.rotate(pygame.transform.scale(ship1,(60,40)),90)
 yellow_ship = pygame.transform.rotate(pygame.transform.scale(ship2,(60,40)),270)
 
-def draw_window(red,yellow):
+bullet_velocity = 5
+fps = 60
+
+def handle_bullets(yellow_bullet,yellow,red_bullet,red):
+    for i in yellow_bullet:
+        i.x -= bullet_velocity
+    for i in red_bullet:
+        i.x += bullet_velocity
+
+
+def draw_window(red,yellow,red_bullet,yellow_bullet):
     screen.blit(sky,(0,0))
     screen.blit(red_ship, (red.x,red.y))
     screen.blit(yellow_ship, (yellow.x,yellow.y))
+    
+    for i in red_bullet:
+        pygame.draw.rect(screen,"red",i)
+    for i in yellow_bullet:
+        pygame.draw.rect(screen, "yellow",i)
+    
+    pygame.display.update()
+
 
 def main():
     yellow = pygame.Rect((900,300,60,40))
     red = pygame.Rect((100,300,60,40))
 
+    red_bullet = []
+    yellow_bullet= []
+
+
     while True:
         for i in pygame.event.get():
             if i.type == pygame.QUIT:
                 run = False
-        
+            if i.type == pygame.KEYDOWN:
+                if i.key == pygame.K_LSHIFT:
+                    bullet = pygame.Rect(red.x+red.width,red.y+red.height // 2 - 2,20,10)
+                    red_bullet.append(bullet)
+                if i.key == pygame.K_RSHIFT:
+                    bullet = pygame.Rect(yellow.x+yellow.width,yellow.y+yellow.height // 2 - 2,20,10)
+                    yellow_bullet.append(bullet)
         keypress = pygame.key.get_pressed()
         yellow_move(keypress,yellow)
+        red_move(keypress,red)
 
-        draw_window(red,yellow)
+        handle_bullets(yellow_bullet,yellow,red_bullet,red)
+        draw_window(red,yellow,red_bullet,yellow_bullet)
         pygame.display.update()
 
 def yellow_move(keypress,yellow):
@@ -42,5 +72,14 @@ def yellow_move(keypress,yellow):
     if keypress[pygame.K_DOWN]:
         yellow.y += 1
 
+def red_move(keypress,red):
+    if keypress[pygame.K_a]:
+        red.x -= 1
+    if keypress[pygame.K_d]:
+        red.x += 1
+    if keypress[pygame.K_w]:
+        red.y -= 1
+    if keypress[pygame.K_s]:
+        red.y += 1
 
 main()
